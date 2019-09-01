880e028801787ff79ee9a676ac42c9ffe3dcf7c9
“Patrick <pctennislessons@gmail.com>
2019-08-31 20:14:43 -0600

    many changes made

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav pills>
+            <router-link to="/"><a>Home</a></router-link>
+            <router-link to="/user"><a>User</a></router-link>
+        </b-nav>
+    </div>
+</template>
